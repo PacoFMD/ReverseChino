a1c60bb585f2a13af3f349c59347fe0f8da03d36
PacoFMD <pacomonfmd@hotmail.com>
2018-08-20 21:05:31 -0500

    Proyecto Arriba

---
+++ Survive/Assets/Script/RutaAuto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RutaAuto : MonoBehaviour {
+    public Transform jugador;
+    public Transform mipos;
+    int velocidad = 3, rotaciónVel = 3;
+
+
+    private void Awake()
+    {
+        //miPos = transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      //  transform.LookAt(jugador,Vector3(position.x, position.y, position.z));
+	}
+}
