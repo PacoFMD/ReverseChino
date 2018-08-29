ed7db0aa906e5c4706f23b8f84f732de89912d87
PacoFMD <pacomonfmd@hotmail.com>
2018-08-28 19:47:25 -0500

    PathFinding
    Uso del Navigation y del NavMesh Agent para encontrar un camino hacia el enemigo

--- Survive/Assets/Script/RutaAuto.cs
+++ Survive/Assets/Script/RutaAuto.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RutaAuto : MonoBehaviour {
-    public Transform jugador;
-    public Transform mipos;
-    int velocidad = 3, rotaciónVel = 3;
-
+ 
+ 
+    [SerializeField]
+    public Transform objetivo;
+    NavMeshAgent navMeshEnemigo;
 
     private void Awake()
     {
@@ -15,7 +17,20 @@ public class RutaAuto : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshEnemigo = this.GetComponent<NavMeshAgent>();
+
+        if(navMeshEnemigo == null)
+        {
+            Debug.Log("Falta el navmesh del objecto " + gameObject.name);
+
+        }
+        else
+        {
+            Debug.Log("Si hay NavMesh");
+            Seguimiento();
+        }
+
+       // jugador = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
@@ -23,4 +38,14 @@ public class RutaAuto : MonoBehaviour {
 
       //  transform.LookAt(jugador,Vector3(position.x, position.y, position.z));
 	}
+
+    void Seguimiento()
+    {
+        if(objetivo != null)
+        {
+            Debug.Log(objetivo.name);
+            Vector3 nuevaPos = objetivo.transform.position;
+            navMeshEnemigo.SetDestination(nuevaPos);
+        }
+    }
 }
