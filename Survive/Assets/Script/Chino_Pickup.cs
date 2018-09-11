86eafc0e6aa035e88dbd3a53bebabd3aade2fcc4
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2018-09-10 20:55:04 -0500

    Primera Edicion de Chino/objetos clave
    Aqui añadi 2 nuevos inputs llamados "crouch" que se usa con la c, y "interact" con la e, añadi 4 scripts, uno es de movimiento de jugador, otro es para rotar items, otro es para recoger items y que hagan cosas al recogerlos y el ultimo es para darle funcionalidad al menu

---
+++ Survive/Assets/Script/Chino_Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chino_Pickup : MonoBehaviour {
+
+    [Header("NombreDelItem")]
+    public string nombreItem;
+
+	void Start () {
+		
+	}
+	
+    public void ItemsClave()
+    {
+        switch (nombreItem)
+        {
+            case "Piston":
+                FindObjectOfType<Chino_FuncionesMenu>().ActivarBoton(0);
+                break;
+            case "Cubo":
+                FindObjectOfType<Chino_FuncionesMenu>().ActivarBoton(1);
+                break;
+            case "Esfera":
+                FindObjectOfType<Chino_FuncionesMenu>().ActivarBoton(2);
+                break;
+        }
+        
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Survive/Assets/Script/Chino_Rotador.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chino_Rotador : MonoBehaviour {
+
+    float VeloRotacion = 20f;
+
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * VeloRotacion * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * VeloRotacion * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, rotY);
+    }
+}
